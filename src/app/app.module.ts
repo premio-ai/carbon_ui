0947f5b7172fb93ebed0b644b185e417db2f9b38
Gaurav L <gauravwebiwork01@gmail.com>
2021-05-04 16:08:45 +0530

    screen 15 to 21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,63 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 import { AddModule, CopyModule } from "@carbon/icons-angular";
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 
-
-import { ArrowLeft16Module } from '@carbon/icons-angular/lib/arrow--left/16';
-
+import { ArrowLeft16Module } from "@carbon/icons-angular/lib/arrow--left/16";
 // carbon-components-angular default imports
-import { UIShellModule,ButtonModule,TabsModule,GridModule,TilesModule,TagModule,ProgressIndicatorModule,BreadcrumbModule,InputModule,ToggleModule,DocumentationModule,DatePickerModule,FileUploaderModule,NumberModule,
-	CheckboxModule,DropdownModule,ListModule,AccordionModule,ContentSwitcherModule,PaginationModule,TableModule,
+import {
+	UIShellModule,
+	ButtonModule,
+	TabsModule,
+	GridModule,
+	TilesModule,
+	TagModule,
+	ProgressIndicatorModule,
+	BreadcrumbModule,
+	InputModule,
+	ToggleModule,
+	DocumentationModule,
+	DatePickerModule,
+	FileUploaderModule,
+	NumberModule,
+	CheckboxModule,
+	DropdownModule,
+	ListModule,
+	AccordionModule,
+	ContentSwitcherModule,
+	PaginationModule,
+	TableModule,
+	NotificationModule,
+	SearchModule
+} from "carbon-components-angular";
+import { Notification20Module } from "@carbon/icons-angular/lib/notification/20";
+import { UserAvatar20Module } from "@carbon/icons-angular/lib/user--avatar/20";
+import { AppSwitcher20Module } from "@carbon/icons-angular/lib/app-switcher/20";
+import { Time16Module } from "@carbon/icons-angular/lib/time/16";
+import { EdtLoop16Module } from "@carbon/icons-angular/lib/edt-loop/16";
+import { UserMultiple16Module } from "@carbon/icons-angular/lib/user--multiple/16";
 
-} from 'carbon-components-angular';
-import { Notification20Module } from '@carbon/icons-angular/lib/notification/20';
-import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
-import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
-import { Time16Module } from '@carbon/icons-angular/lib/time/16';
-import { EdtLoop16Module } from '@carbon/icons-angular/lib/edt-loop/16';
-import { UserMultiple16Module } from '@carbon/icons-angular/lib/user--multiple/16';
+import { HeaderComponent } from "./header/header.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { ChallangeTileComponent } from "./challange-tile/challange-tile.component";
+import { RequestService } from "./request.service";
+import { ChallangePageComponent } from "./challange-page/challange-page.component";
+import { ChallaneThirdStepComponent } from "./challane-third-step/challane-third-step.component";
+import { ChallangeSecondStepComponent } from "./challange-second-step/challange-second-step.component";
+import { ChallangeFirstStepComponent } from "./challange-first-step/challange-first-step.component";
+import { ChallangeFourthStepComponent } from "./challange-fourth-step/challange-fourth-step.component";
+import { InsurerComponent } from "./insurer/insurer.component";
+import { OverviewComponent } from "./overview/overview.component";
+import { DataComponent } from "./data/data.component";
+import { ActivityComponent } from "./activity/activity.component";
 
-import { HeaderComponent } from './header/header.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ChallangeTileComponent } from './challange-tile/challange-tile.component';
-import { RequestService } from './request.service';
-import { ChallangePageComponent } from './challange-page/challange-page.component';
-import { ChallaneThirdStepComponent } from './challane-third-step/challane-third-step.component';
-import { ChallangeSecondStepComponent } from './challange-second-step/challange-second-step.component';
-import { ChallangeFirstStepComponent } from './challange-first-step/challange-first-step.component';
-import { ChallangeFourthStepComponent } from './challange-fourth-step/challange-fourth-step.component';
-import { InsurerComponent } from './insurer/insurer.component';
-import { OverviewComponent } from './overview/overview.component';
-import { DataComponent } from './data/data.component';
-import { ActivityComponent } from './activity/activity.component';
-
-import { ModelComponent } from './model/model.component';
+import { ModelComponent } from "./model/model.component";
+import { ModelReportComponent } from "./model-report/model-report.component";
+import { PreviewComponent } from './preview/preview.component';
 
 @NgModule({
 	declarations: [
@@ -53,8 +74,12 @@ import { ModelComponent } from './model/model.component';
 		OverviewComponent,
 		DataComponent,
 		ActivityComponent,
-	
-		ModelComponent
+
+		ModelComponent,
+
+		ModelReportComponent,
+
+		PreviewComponent,
 	],
 	imports: [
 		BrowserModule,
@@ -73,8 +98,10 @@ import { ModelComponent } from './model/model.component';
 		TilesModule,
 		TagModule,
 		HttpClientModule,
-		ArrowLeft16Module, ProgressIndicatorModule,
-		BreadcrumbModule, InputModule,
+		ArrowLeft16Module,
+		ProgressIndicatorModule,
+		BreadcrumbModule,
+		InputModule,
 		ToggleModule,
 		DocumentationModule,
 		DatePickerModule,
@@ -87,13 +114,14 @@ import { ModelComponent } from './model/model.component';
 		ContentSwitcherModule,
 		PaginationModule,
 		TableModule,
-		
-	
+		NotificationModule,
+		SearchModule,
+				
 		Time16Module,
 		EdtLoop16Module,
-		UserMultiple16Module
+		UserMultiple16Module,
 	],
 	bootstrap: [AppComponent],
-	providers: [RequestService]
+	providers: [RequestService],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/model-report/model-report.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-model-report',
+  templateUrl: './model-report.component.html',
+  styleUrls: ['./model-report.component.scss']
+})
+export class ModelReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    showClose: true
+  }
+
+}
