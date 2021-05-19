31cb143207780eccb6d62fb931845a30bbe1fd70
Gaurav L <gauravwebiwork01@gmail.com>
2021-05-19 19:58:53 +0530

    chart

--- src/app/request.service.ts
+++ src/app/request.service.ts
@@ -8,8 +8,8 @@ import { map, catchError} from 'rxjs/operators';
 })
 
 export class RequestService {
-  baseUrl : string = "http://localhost:3000/api/";
-  // baseUrl: string = "https://meanapp01.herokuapp.com/api/";
+  // baseUrl : string = "http://localhost:3000/api/";
+  baseUrl: string = "https://meanapp01.herokuapp.com/api/";
   constructor(private http:HttpClient) { }
 
   get(uri, params): Observable<any> {
