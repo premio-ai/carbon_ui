42dd5cd9ba12d94112dd6e7b22f16b20b42819c5
Gaurav <gauravwebiwork01@gmail.com>
2021-05-19 18:37:59 +0530

    model page API code merge

--- src/app/challange-first-step/challange-first-step.component.ts
+++ src/app/challange-first-step/challange-first-step.component.ts
@@ -13,20 +13,35 @@ export class ChallangeFirstStepComponent implements OnInit {
     title: string,
     description: string,
     objective: string,
+    // categoryType: string,
     endDate: string
   }
   titleError: boolean
   descriptionError: boolean
   objectiveError: boolean
   endDateError: boolean
+  category: any[]
 
   ngOnInit() {
     this.stepOne = {
       title: "",
       description: "",
       objective: "",
+      // categoryType: "",
       endDate: ""
     };
+
+    this.category = [
+      { content: 'Efficiency' },
+      { content: 'Claims' },
+      { content: 'Customers' },
+      { content: 'Bare Bones' },
+    ]
+
+  }
+
+  selected(category) {
+    // this.stepOne.categoryType = category.content.item
   }
 
   minDate() {
@@ -35,26 +50,26 @@ export class ChallangeFirstStepComponent implements OnInit {
 
   nextStep() {
     // TODO : uncomment
-    this.goNext.emit(this.stepOne)
+    // this.goNext.emit(this.stepOne)
 
     var date_regex = /^(0[1-9]|1\d|2\d|3[01])\/(0[1-9]|1[0-2])\/(19|20)\d{2}$/;
 
-    // if (this.stepOne.title.length>0 && this.stepOne.description.length>0 && this.stepOne.objective.length>0 && this.stepOne.endDate.length>0) {
-    //   this.goNext.emit(this.stepOne)
-    // } else {
-    //   if (this.stepOne.title.length == 0) {
-    //     this.titleError = true
-    //   }
-    //   if (this.stepOne.description.length == 0) {
-    //     this.descriptionError = true
-    //   }
-    //   if (this.stepOne.objective.length == 0) {
-    //     this.objectiveError = true
-    //   }
-    //   if (this.stepOne.endDate.length == 0 && !(date_regex.test(this.stepOne.endDate))) {
-    //     this.endDateError = true
-    //   }
-    // }
+    if (this.stepOne.title.length>0 && this.stepOne.description.length>0 && this.stepOne.objective.length>0 && this.stepOne.endDate.length>0) {
+      this.goNext.emit(this.stepOne)
+    } else {
+      if (this.stepOne.title.length == 0) {
+        this.titleError = true
+      }
+      if (this.stepOne.description.length == 0) {
+        this.descriptionError = true
+      }
+      if (this.stepOne.objective.length == 0) {
+        this.objectiveError = true
+      }
+      if (this.stepOne.endDate.length == 0 && !(date_regex.test(this.stepOne.endDate))) {
+        this.endDateError = true
+      }
+    }
   }
 
 }
